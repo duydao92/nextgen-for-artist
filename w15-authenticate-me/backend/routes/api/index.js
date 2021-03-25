b0b1d35a8e558d356b7cebeff1d1ff73fadb1793
duy <ddao0892@gmail.com>
2021-03-25 16:38:32 -0600

    “logout”

--- w15-authenticate-me/backend/routes/api/index.js
+++ w15-authenticate-me/backend/routes/api/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
+
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+// const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+// router.use('users', usersRouter);
 
 
 router.get('/set-token-cookie', asyncHandler(async (req, res) => {
