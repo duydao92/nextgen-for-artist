15021144caab3a225d0eda7f881e80fdc1bbfabd
duy <ddao0892@gmail.com>
2021-03-25 17:42:58 -0600

    “user-auth-finished”

--- w15-authenticate-me/backend/routes/api/users.js
+++ w15-authenticate-me/backend/routes/api/users.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { validateSignup } = require("../../utils/validation");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
+
+
+
 //Sign up
 router.post(
   "",
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, password, username });
